7feb1064a48ae63ee2b119ee77161badf88597d8
rakochyi <80195285+rakochyi@users.noreply.github.com>
2021-05-12 07:22:08 +0300

    Add files via upload

---
+++ code/Elogic/Provider/InstallData.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Elogic\Provider\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Elogic\Provider\Model\Attribute\Source\ProviderAttr;
+
+class InstallData implements DataPatchInterface, PatchRevertableInterface
+{
+    const PRODUCT_ATTRIBUTE = 'providers_category';
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+
+    /**
+     * InstallData constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+
+    /**
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            self::PRODUCT_ATTRIBUTE,
+            [
+                'type' => 'varchar',
+                'label' => 'Providers Category',
+                'input' => 'select',
+                'source' => ProviderAttr::class,
+                'frontend' => \Elogic\Provider\Model\Attribute\Frontend\ProviderAttr::class,
+                'required' => false,
+                'backend' => ArrayBackend::class,
+                'sort_order' => '30',
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'default' => null,
+                'visible' => true,
+                'user_defined' => true,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'unique' => false,
+                'apply_to' => '',
+                'group' => 'General',
+                'used_in_product_listing' => true,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'option' => ''
+            ]
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(Product::ENTITY, self::PRODUCT_ATTRIBUTE);
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+
+    /**
+     * @return array
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+}
